0cee2c14a34d706ded094bd078d76534262b12d3
Carey Williams <carey.williams@arm.com>
2025-09-11 13:37:39 +0100

    Initial commit

---
+++ scripts/flatbuffer-gen.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# SPDX-FileCopyrightText: Copyright 2025 Arm Limited and/or its affiliates <open-source-office@arm.com>
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License v2.0
+# See http://www.apache.org/licenses/LICENSE-2.0 for license information.
+set -euo pipefail
+
+cd "$(dirname "$0")"/..
+
+SCHEMA_DIR=schema
+BASE_TARGET_DIR=src/tosa_adapter_model_explorer
+
+for schema in "$SCHEMA_DIR"/*.fbs; do
+  schema_basename=$(basename "$schema" .fbs)
+  target_dir="$BASE_TARGET_DIR/$schema_basename"
+
+  mkdir -p "$target_dir"
+  flatc --python --gen-onefile --gen-object-api --python-typing \
+    -o "$target_dir" "$schema"
+done
